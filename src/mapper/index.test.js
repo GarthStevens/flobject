70e4ece47f4844b64db884fb36d9d3dee3e26f61
Garth Stevens <garth@mudbath.com.au>
2017-11-17 16:49:19 +1100

    tests

--- src/mapper/index.test.js
+++ src/mapper/index.test.js
@@ -3,11 +3,39 @@ import {Mapper} from './index';
 
 describe('Mapper', () => {
   describe('map', () => {
-    it('it should map the object', () => {
+    it('it should map the object fields', () => {
 
+      const mapper = new Mapper();
 
+      const person = {
+        firstname: 'Bill',
+        lastname: 'Smith',
+        job: {
+          title: 'IT',
+          roles: ['Dev', 'Admin', 'DevOps'],
+          manager: {
+            firstname: 'Sam',
+            lastname: 'S',
+          }
+        }
+      };
 
-      // chai.expect(objectMap.maps['field1']).to.not.be.undefined;
+      const expected = {
+        firstName: 'Bill',
+        lastName: 'Smith'
+      };
+
+      mapper
+        .register('person')
+        .field('firstName', a => a.sourceObject.firstname)
+        .field('lastName', a => a.sourceObject.lastname);
+        // .field('career', a => a.mapper('job', a.sourceObject.job));
+
+        console.log(mapper)//eslint-disable-line
+
+      const result = mapper.map('person', person);
+
+      chai.expect(result).to.deep.equals(expected);
       // chai.expect(objectMap.maps['field2']).to.not.be.undefined;
     });
   })
